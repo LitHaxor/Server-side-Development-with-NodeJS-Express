de2240cac9e2f14742236a80b0a73c834a2d0937
lithaxor <officials.hasan@gmail.com>
2020-07-01 00:01:01 +0600

    Callbacks and Error

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let rect = require('./rectangle');
-function solveReact(l,b){
-    console.log("Solving for rectangle with l ="+ l + " b=" +b);
-    if(l <= 0 || b<= 0){
-        console.log("reactangle dimention should be greater than zero ");
-        
-    }
-    else 
-    {
-        console.log("area of the reactangle is " + rect.area(l,b));
-        console.log("Perimeter of the react:" + rect.perimeter(l,b));
-    }
-}
-
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 solveReact(3,5);
 solveReact(5,5);
 solveReact(-4,5);
