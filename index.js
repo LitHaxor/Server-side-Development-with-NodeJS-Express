1519e81bc4492a8cd36412c323b044137392e8ff
lithaxor <officials.hasan@gmail.com>
2020-07-01 00:08:07 +0600

    Node callback and error handling

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let rect = require('./rectangle');
+
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = "
                 + l + " and b = " + b);
@@ -13,8 +15,8 @@ function solveRect(l,b) {
         }
     });
     console.log("This statement after the call to rect()");
-}; 
+};
 
-solveReact(3,5);
-solveReact(5,5);
-solveReact(-4,5);
+solveRect(3,5);
+solveRect(5,5);
+solveRect(-4,5);
