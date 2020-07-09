3c7e06fdbb9a857914f3df6310d87b7c4958f0fc
lithaxor <officials.hasan@gmail.com>
2020-07-10 00:19:23 +0600

    mongoose part 3

--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+const commentSchema = new Schema({
+    rating : {
+        type : Number,
+        min: 1,
+        max : 5,
+        require : true
+    },
+    comment : {
+        type : String,
+        required : true
+    },
+    author :{
+        type: String,
+        required : true
+    }
+},{
+    timestamps: true
+}
+)
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +31,8 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    }, 
+    comments : [commentSchema]
 },{
     timestamps: true
 });
